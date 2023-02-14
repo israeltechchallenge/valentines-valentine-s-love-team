5071468009dc594e471fdfb072d2dace11760b07
isokolov <illya.sokolov82@gmail.com>
2023-02-14 20:36:34 +0200

    some css styling for about component

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -6,11 +6,11 @@ import pic3 from "../images/illya.jpg";
 
 function About() {
   return (
-    <div>
+    <div className="about-page">
       <h1>About Team</h1>
       <div>
         <h2>Ohad</h2>
-        <img src={pic1} alt="" className="pics-team" />
+        <img src={pic1} alt="" className="pics-team person1" />
         <p>
           Ohad Lichtenstein. Ohad has been an ITC student for almost 5 months.
           He is married and has 2 dogs and a cat.
@@ -18,7 +18,7 @@ function About() {
       </div>
       <div>
         <h2>Ilana</h2>
-        <img src={pic2} alt="" className="pics-team" />
+        <img src={pic2} alt="" className="pics-team person2" />
         <p>
           Ilana Katzin. Ilana has been an ITC student for almost 5 months. She
           lives in Jerusalem and works in QA.
@@ -26,7 +26,7 @@ function About() {
       </div>
       <div>
         <h2>Illya</h2>
-        <img src={pic3} alt="" className="pics-team" />
+        <img src={pic3} alt="" className="pics-team person1" />
         <p>
           illya Sokolov. Illya has been an ITC student for almost 5 months. He
           studied computer sciences and obtains new skills as a Full stack
