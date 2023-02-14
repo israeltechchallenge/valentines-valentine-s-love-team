1cf8bda48212a42d9706adac05804c26a135675d
Ilana Katzin <ilana.katzin@gmail.com>
2023-02-14 19:27:17 +0200

    merges with main branch

--- src/App.js
+++ src/App.js
@@ -1,26 +1,18 @@
 
-<<<<<<< HEAD
 import FindLove from './components/FindLove/FindLove';
-=======
 import { useState } from 'react';
 import Login from './components/Login';
 import UserContext from './contexts/UserContext';
->>>>>>> main
 import './css/App.css';
 
 function App() {
   const [user, setUser] = useState({})
   return (
-<<<<<<< HEAD
-    <div className="App">
-      <FindLove />
-    </div>
-=======
     <UserContext.Provider value={{user, setUser}}>
       <div className="App">
+        <FindLove />
       </div>
     </UserContext.Provider>
->>>>>>> main
   );
 }
 
